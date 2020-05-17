b481292633d576001f0a8be7cd3a9c124d72bdf6
Lix <1335903787@qq.com>
2020-05-17 21:22:15 +0800

    pipeline_20200517_4
    补充

--- MEM_WB.v
+++ MEM_WB.v
@@ -1,1 +1,25 @@
+module MEM_WB(
+input clk,
+input reset,
+input controlsIn,
+input[15:0] result2In,
+input[2:0] Reg1In,
+output controlsOut,
+output[15:0] result2Out,
+output[2:0] Reg1Out
+);
+
+reg[19:0] R;
+assign controlsOut=R[19];
+assign result2Out=R[18:3];
+assign Reg1Out=R[2:0];
+
+always@(posedge clk)
+if (reset) R<=0;
+else begin
+R[19]<=controlsIn;
+R[18:3]<=result2In;
+R[2:0]<=Reg1In; 
+end
+endmodule
 
